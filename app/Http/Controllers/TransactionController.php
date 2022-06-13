531f3b6951e082c5a2c5cb86d8289a6cef2c5673
AymanAbubakr <aymanmawlan1014@gmail.com>
2022-06-14 01:56:14 +0300

    adding transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Http\Requests\TransactionRequest;
+use Illuminate\Support\Facades\DB;
+use App\Models\Journal;
 
 class TransactionController extends Controller
 {
@@ -14,7 +17,11 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        //
+        $transactions = Transaction::all()->where('deleted', 0);
+
+        return response()->json([
+            'transactions' => $transactions
+        ]);
     }
 
     /**
@@ -33,9 +40,107 @@ class TransactionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TransactionRequest $transactionRequest)
     {
-        //
+        try {
+            if ($transactionRequest->credit_account_id == $transactionRequest->debit_account_id) {
+                return response()->json([
+                    'message' => "Credit and Debit account cannot be same!",
+                ], 400);
+            }
+
+            DB::beginTransaction();
+
+            $journal = Journal::create([
+                'credit_account_id' => $transactionRequest->credit_account_id,
+                'debit_account_id' => $transactionRequest->debit_account_id,
+                'amount' => $transactionRequest->credit_amount,
+                'comment' => $transactionRequest->comment,
+            ]);
+
+            Transaction::insert([
+                [
+                    'credit_account_id' => $transactionRequest->credit_account_id,
+                    'debit_account_id' => $transactionRequest->debit_account_id,
+                    'credit_amount' => $transactionRequest->credit_amount,
+                    'debit_amount' => 0,
+                    'journal_id' => $journal->id,
+                    'comment' => $transactionRequest->comment,
+                ],
+                [
+                    'credit_account_id' => $transactionRequest->credit_account_id,
+                    'debit_account_id' => $transactionRequest->debit_account_id,
+                    'credit_amount' => 0,
+                    'debit_amount' =>  $transactionRequest->credit_amount,
+                    'journal_id' => $journal->id,
+                    'comment' => $transactionRequest->comment,
+                ],
+            ]);
+
+            DB::commit();
+
+
+            return response()->json([
+                'message' => "Transaction Created successfully!",
+            ], 200);
+        } catch (\Exception $exp) {
+            DB::rollBack();
+            return response([
+                'message' => $exp->getMessage(),
+            ], 400);
+        }
+    }
+
+    public function revertTransaction($id)
+    {
+        try {
+            $transaction = Transaction::find($id);
+
+            if ($transaction->is_null) {
+                return response()->json([
+                    'message' => "Transaction not found!",
+                ], 404);
+            }
+
+            DB::beginTransaction();
+
+            $journal = Journal::create([
+                'credit_account_id' => $transaction->credit_account_id,
+                'debit_account_id' => $transaction->debit_account_id,
+                'amount' => $transaction->credit_amount,
+                'comment' => $transaction->comment,
+            ]);
+    
+            Transaction::insert([
+                [
+                    'credit_account_id' => $transaction->debit_account_id,
+                    'debit_account_id' => $transaction->credit_account_id,
+                    'credit_amount' => $transaction->credit_amount,
+                    'debit_amount' => 0,
+                    'journal_id' => $journal->id,
+                    'comment' => $transaction->comment,
+                ],
+                [
+                    'credit_account_id' => $transaction->debit_account_id,
+                    'debit_account_id' => $transaction->credit_account_id,
+                    'credit_amount' => 0,
+                    'journal_id' => $journal->id,
+                    'debit_amount' =>  $transaction->credit_amount,
+                    'comment' => $transaction->comment,
+                ],
+            ]);
+
+            DB::commit();
+    
+            return response()->json([
+                'message' => "Transaction Reverted successfully!",
+            ], 200);
+        } catch (\Exception $exp) {
+            DB::rollBack();
+            return response([
+                'message' => $exp->getMessage(),
+            ], 400);
+        }
     }
 
     /**
