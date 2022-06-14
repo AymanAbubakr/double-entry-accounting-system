e0f1291990a58556259fd4398d5b1835412ca93c
AymanAbubakr <aymanmawlan1014@gmail.com>
2022-06-15 00:23:07 +0300

    creating new endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactTypeController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TypeAccountController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,15 +32,28 @@ Route::post('/users', [UserController::class, 'store'])->middleware('auth:sanctu
 Route::put('/users/{user}', [UserController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth:sanctum');
 
-
 Route::get('/transactions', [TransactionController::class, 'index'])->middleware('auth:sanctum');
 Route::post('/transactions', [TransactionController::class, 'store'])->middleware('auth:sanctum');
 Route::put('/transactions/revert/{journalId}', [TransactionController::class, 'revertTransaction'])->middleware('auth:sanctum');
 Route::put('/transactions/{transaction}', [TransactionController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy'])->middleware('auth:sanctum');
 
+Route::get('/contacts', [ContactController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/contacts', [ContactController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/contacts/{contact}', [ContactController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->middleware('auth:sanctum');
+
+Route::get('/contactTypes', [ContactTypeController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/contactTypes', [ContactTypeController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/contactTypes/{contactType}', [ContactTypeController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/contactTypes/{contactType}', [ContactTypeController::class, 'destroy'])->middleware('auth:sanctum');
+
+Route::get('/typeAccount', [TypeAccountController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/typeAccount', [TypeAccountController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/typeAccount/{typeAccount}', [TypeAccountController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/typeAccount/{typeAccount}', [TypeAccountController::class, 'destroy'])->middleware('auth:sanctum');
+
 
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
-
