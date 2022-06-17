7409e84a6082aa7635514769c23b6ea7659b48f3
AymanAbubakr <aymanmawlan1014@gmail.com>
2022-06-17 10:57:11 +0300

    Adding getOne funciton in account.php

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -10,11 +10,23 @@ class Account extends Model
     use HasFactory;
     protected $table = 'accounts';
     protected $primaryKey = 'id';
-    protected $fillable = ['name', 'parent_id', 'deleted'];
+    protected $fillable = ['name', 'parent_id', 'parent_tree_ids', 'deleted'];
 
 
     public static function getAll()
     {
         return Account::all()->where('deleted', 0);
     }
+
+    public static function getOne($accountId)
+    {
+        return Account::where([
+            ['id', $accountId],
+            ['deleted', 0]
+        ])->first();
+    }
+
+    protected $casts = [
+        'parent_tree_ids' => 'array',
+    ];
 }
